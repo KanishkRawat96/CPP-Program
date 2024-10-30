5d76c7cde0049233edbb23d2173e4eb9b06d3150
KanishkRawat96 <144546979+KanishkRawat96@users.noreply.github.com>
2024-10-30 18:25:33 +0530

    Update frndClass2.cpp

--- frndClass2.cpp
+++ frndClass2.cpp
@@ -5,10 +5,14 @@ class A
 {
     private:
     int a=76;
+    protected:
+    double gh=65;
     friend class F;
 };
 class B
-{
+{ 
+    private:
+    int s=89;
     protected:
     int b=3;
     friend class F;
@@ -17,6 +21,8 @@ class C
 {
     private:
     int c=9;
+    protected:
+    double x=56;
     friend class F;
 };
 
@@ -25,6 +31,9 @@ class F
     public:
     void access(A r1,B r2,C r3)
     {
+        cout<<r1.gh<<endl;
+        cout<<r2.s<<endl;
+        cout<<r3.x<<endl;
         cout<<r1.a<<endl;
         cout<<r2.b<<endl;
         cout<<r3.c<<endl;
